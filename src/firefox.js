5f323ae179c356a138de8741dbe75d070f6788a2
John Jones <john@exthilion.org>
2021-11-20 12:55:13 -0800

    grant explicit checks:write to lint workflow

--- src/firefox.js
+++ src/firefox.js
@@ -8,7 +8,7 @@ module.exports = new Extension(async req => {
   const signed = await signer.sign();
 
   // aws4fetch removes Host, and lemme tell you, AWS does not care for that.
-  // They give a very opaque 400 error with out a body to explain why.
+  // They give a very opaque 400 error without a body to explain why.
   const requestHeaders = [
     ['Host', signer.url.host],
     ...signed.headers,
