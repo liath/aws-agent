80a045074bb594e60cf124a4b41afc32199fb4d4
gongy <yi.gong@microfocus.com>
2022-04-10 08:16:19 +0800

    Fix the chrome invalid arguments issue and others 1. Chrome doesn't allow "Host" and "Content-Length" headers to modify. 2. Fix the wrong body object when singing 3. Add an optional session token which used in the assumed short term token

--- src/dialog.js
+++ src/dialog.js
@@ -3,17 +3,19 @@ const browser = require('webextension-polyfill');
 const keyupHandler = () => browser.storage.sync.set({
   accessKeyId: document.getElementById('keyid').value,
   secretAccessKey: document.getElementById('secret').value,
+  sessionToken: document.getElementById('session_token').value,
 });
 
 document.addEventListener('DOMContentLoaded', async () => {
   const creds = await browser.storage.sync.get({
     accessKeyId: '',
     secretAccessKey: '',
+    sessionToken: '',
   });
 
   document.getElementById('keyid').value = creds.accessKeyId;
   document.getElementById('secret').value = creds.secretAccessKey;
-
+  document.getElementById('session_token').value = creds.sessionToken;
   document.querySelectorAll('input').forEach(input => {
     input.addEventListener('keydown', keyupHandler);
     input.addEventListener('keyup', keyupHandler);
